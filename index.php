5c11f971ce48c0e643b77280e0fddb6d45c9b13f
danilfg <danilfg@yahoo.com>
2021-01-06 17:31:27 +0300

    test html

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<?php
-include("header.php");
-include("content.php");
-include("footer.php");
+
+
+<title>Пример сайта на php</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<div class="header">
+<h2>Шапка</h2>
+</div>
+ <div class="pages">
+<div class="content">
+<h2>Контент</h2>
+</div>
+<div class="sidebar">
+    <ul>
+        <li> <a href="file1.php">Раздел 1</a></li>
+        <li><a href="file2.php">Раздел 2</a></li>
+        <li><a href="file3.php">Раздел 3</a></li>
+        <li><a href="file4.php">Раздел 4</a></li>
+        <li><a href="file5.php">Раздел 5</a></li>
+    </ul>
+</div>
+ <div class="foot">
+</div>
+</div>
+ <div class="footer">
+<h2>Подвал</h2>
+</div>
+</body>
+</html>
